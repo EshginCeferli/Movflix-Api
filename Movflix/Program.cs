159c3211b74d03ad10196b2963ce05fcd2dab1bc
EshginCeferli <eshginij@code.edu.az>
2023-01-01 15:12:47 +0400

    Backup

--- Movflix/Program.cs
+++ Movflix/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddDbContextPool<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
